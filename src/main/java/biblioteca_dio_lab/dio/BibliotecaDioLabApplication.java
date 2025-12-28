6ec62a135f70485aacc9d0b960d488289d8310eb
BrenoMelo <brenoigor123@gmail.com>
2025-12-28 16:57:34 -0300

    init spring project

---
+++ src/main/java/biblioteca_dio_lab/dio/BibliotecaDioLabApplication.java
@@ -0,0 +1,13 @@
+package biblioteca_dio_lab.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDioLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDioLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biblioteca_dio_lab/dio/BibliotecaDioLabApplicationTests.java
@@ -0,0 +1,13 @@
+package biblioteca_dio_lab.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDioLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
